1f3851c0973512336ce29143a4f302d74414c5bc
Annie Huang <annie.hn@gmail.com>
2025-12-11 21:19:34 +1100

    Add Hero into App.vue

--- src/App.vue
+++ src/App.vue
@@ -1,8 +1,10 @@
 <script setup>
 import Navbar from '@/components/Navbar.vue';
+import Hero from '@/components/Hero.vue';
 </script>
 
 <template>
   <!--  <h1 class="text-2xl">Vue Jobs</h1>-->
   <Navbar />
+  <Hero />
 </template>
