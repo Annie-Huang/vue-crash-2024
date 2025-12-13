cf73449c17b7eb919463880d44299851dc27d87a
Annie Huang <annie.hn@gmail.com>
2025-12-13 19:20:09 +1100

    Retrieve and use content in jobs.json file

--- src/components/JobListings.vue
+++ src/components/JobListings.vue
@@ -1,3 +1,10 @@
-<script setup></script>
+<script setup>
+import jobData from '@/jobs.json';
+import { ref } from 'vue';
+
+// Similar to make this useState in react
+const jobs = ref(jobData);
+console.log(jobs.value);
+</script>
 
 <template>Job Listings</template>
