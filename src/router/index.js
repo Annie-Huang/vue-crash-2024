be8898d3871dbdfd9878083d474945d00a5a05c1
Annie Huang <annie.hn@gmail.com>
2025-12-14 16:42:15 +1100

    Change the href in Navbar.vue to related path so we can use in them in the router

--- src/router/index.js
+++ src/router/index.js
@@ -1,3 +1,4 @@
+// Note: all these will be created for you if you choose to add router when you create vue app
 import { createRouter, createWebHistory } from 'vue-router';
 import HomeView from '@/views/HomeView.vue';
 
