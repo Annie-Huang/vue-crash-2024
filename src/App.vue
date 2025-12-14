2a64de04103d74a7a68b10ce53f75fccc54a4339
Annie Huang <annie.hn@gmail.com>
2025-12-14 16:22:12 +1100

    Create HomeView.vue and shift Hero, HomeCards, JobListings from App.vue to there.

--- src/App.vue
+++ src/App.vue
@@ -1,15 +1,8 @@
 <script setup>
 import Navbar from '@/components/Navbar.vue';
-import Hero from '@/components/Hero.vue';
-import HomeCards from '@/components/HomeCards.vue';
-import JobListings from '@/components/JobListings.vue';
 </script>
 
 <template>
   <!--  <h1 class="text-2xl">Vue Jobs</h1>-->
   <Navbar />
-  <!--  <Hero title="Test Title" subtitle="Test Subtitle" />-->
-  <Hero />
-  <HomeCards />
-  <JobListings :limit="3" :showButton="true" />
 </template>
