bcc9af66940ddd7329774c85f35900e41feb3499
Annie Huang <annie.hn@gmail.com>
2025-12-14 14:35:13 +1100

    Copy the View All Jobs button from _theme_files/index.html into JobListings.vue

--- src/components/JobListings.vue
+++ src/components/JobListings.vue
@@ -27,4 +27,12 @@ const jobs = ref(jobData);
       </div>
     </div>
   </section>
+
+  <section class="m-auto max-w-lg my-10 px-6">
+    <a
+      href="jobs.html"
+      class="block bg-black text-white text-center py-4 px-6 rounded-xl hover:bg-gray-700"
+      >View All Jobs</a
+    >
+  </section>
 </template>
