64a042793a2716503af33d6382399543ca759878
Annie Huang <annie.hn@gmail.com>
2025-12-15 20:04:50 +1100

    Show spinner if it's loading state

--- src/components/JobListings.vue
+++ src/components/JobListings.vue
@@ -4,6 +4,7 @@ import { RouterLink } from 'vue-router';
 import { ref, defineProps, onMounted, reactive } from 'vue';
 import JobListing from '@/components/JobListing.vue';
 import axios from 'axios';
+import PulseLoader from 'vue-spinner/src/PulseLoader.vue';
 
 defineProps({
   limit: Number,
@@ -42,6 +43,11 @@ onMounted(async () => {
       <h2 class="text-3xl font-bold text-green-500 mb-6 text-center">
         Browse Jobs
       </h2>
+      <!-- Show loading spinner while loading is true -->
+      <div v-if="state.isLoading" class="text-center text-gray-500 py-6">
+        <PulseLoader />
+      </div>
+
       <div class="grid gird-cols-1 md:grid-cols-3 gap-6">
         <!--        <JobListing
             v-for="job in jobs.slice(0, limit || jobs.length)"
