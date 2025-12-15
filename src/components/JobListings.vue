08cd00eef24784cd4618bf8fdfcfc9c1e9d0deee
Annie Huang <annie.hn@gmail.com>
2025-12-15 19:46:18 +1100

    Make axios call on onMounted function and confirmed in network tab that the /jobs call is made

--- src/components/JobListings.vue
+++ src/components/JobListings.vue
@@ -1,8 +1,9 @@
 <script setup>
 import { RouterLink } from 'vue-router';
 // import jobData from '@/jobs.json';
-import { ref, defineProps } from 'vue';
+import { ref, defineProps, onMounted } from 'vue';
 import JobListing from '@/components/JobListing.vue';
+import axios from 'axios';
 
 defineProps({
   limit: Number,
@@ -16,6 +17,15 @@ defineProps({
 // const jobs = ref(jobData);
 const jobs = ref([]);
 // console.log(jobs.value);
+
+onMounted(async () => {
+  try {
+    const response = await axios.get('http://localhost:5000/jobs');
+    jobs.value = response.data;
+  } catch (error) {
+    logger.error('Error fetching jobs', error);
+  }
+});
 </script>
 
 <template>
