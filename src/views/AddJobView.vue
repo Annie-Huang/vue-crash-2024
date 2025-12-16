c18d091a1a666652ba0f46db548bba9e67f4a8d4
Annie Huang <annie.hn@gmail.com>
2025-12-16 20:27:55 +1100

    Create the axio post call and after we add the new job, remove to the new job. Tested and success

--- src/views/AddJobView.vue
+++ src/views/AddJobView.vue
@@ -1,5 +1,7 @@
 <script setup>
 import { reactive } from 'vue';
+import axios from 'axios';
+import router from '@/router';
 
 const form = reactive({
   type: 'Full-Time',
@@ -30,7 +32,16 @@ const handleSubmit = async () => {
       contactPhone: form.company.contactPhone,
     },
   };
-  console.log(newJob);
+  // console.log(newJob);
+
+  try {
+    const response = await axios.post('/api/jobs', newJob);
+    // @todo - show toast
+    router.push(`/jobs/${response.data.id}`);
+  } catch (error) {
+    console.error('Error fetching job', error);
+    // @todo - show toast
+  }
 };
 </script>
 
